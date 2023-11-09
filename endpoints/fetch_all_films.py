7868dd6d2bb8fc97a4eb614df373dd911c44d227
Craig <craigtipple81@gmail.com>
2023-11-09 12:10:51 +0000

    Completed endpoint: get_film_by_title along with test. All working.

--- endpoints/fetch_all_films.py
+++ endpoints/fetch_all_films.py
@@ -1,11 +1,30 @@
+from flask import request
+
 def fetch_all_films(connection):
+    film_title = request.args.get('title')
     with connection: 
+        
         with connection.cursor() as cursor:
-            cursor.execute('SELECT * FROM films;')
-            films=cursor.fetchall()
-            if films:
+            if film_title:
+                cursor.execute('SELECT * FROM films WHERE title ILIKE %s;',('%' + film_title + '%',))
+                film_by_title=cursor.fetchall()
+
                 result = []
-                for film in films:
-                    result.append({"id": film[3], "original_title": film[5], "overview": film[6], "poster_path": film[8],
-                                   "release_date": film[9], "vote_average": film[12], "vote_count": film[13]})
-    return {"films": result}
+                for film in film_by_title:
+                        result.append({"id": film[3], "title": film[10], "overview": film[6], "poster_path": film[8],
+                                    "release_date": film[9], "vote_average": film[12], "vote_count": film[13]})
+                if len(result) <= 0:
+                        return {"message": "We couldn't find this film."}
+                else:     
+                        return {"films": result}
+
+            else:
+                cursor.execute('SELECT * FROM films;')
+                films=cursor.fetchall()
+
+                if films:
+                    result = []
+                    for film in films:
+                        result.append({"id": film[3], "title": film[10], "overview": film[6], "poster_path": film[8],
+                                    "release_date": film[9], "vote_average": film[12], "vote_count": film[13]})
+                return {"films": result}
